47dc5f9ffb743509e09286e4de1e1ae53e83754c
ShreyaNK409 <78596107+ShreyaNK409@users.noreply.github.com>
2021-04-13 18:15:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+canvas=
+  document.getElementById("MYCanvas");
+ctx= canvas.getContext("2d")
+
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.lineWidht="1";
+ctx.rect(150,143,430,200);
+ctx.strokeStyle();
